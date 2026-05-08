dcf0aed344fba5e7a9c361cd4de007caa5e14a82
Alius <151633979+AliusSidlauskas@users.noreply.github.com>
2026-05-08 10:59:21 +0300

    Update index.tsx
    Added email

--- pages/contacts/index.tsx
+++ pages/contacts/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import PageTemplate from "../../components/PageTemplate/PageTemplate";
 import styles from "../contacts/contacts.module.css";
 
@@ -7,6 +6,17 @@ const contacts = () => {
     <PageTemplate>
       <div className={styles.main}>
         <div className={styles.box}>
+
+          <h2>Telefonas: </h2>
+
+          <div className={styles.phones}>
+            <a href="tel:+37067032769">Vadybininkas: +370 67 032 769</a>
+            <a href="tel:+37060655648">Direktorius: +370 60 655 648</a>
+          </div>
+          
+          <h2>El. paštas:</h2>
+          <a href="mailto:info@santenora.lt">info@santenora.lt</a>
+
           <h2>Parduotuvės adresas:</h2>
           <a
             href="https://www.google.com/maps/search/?api=1&query=Kretinga,+Salantų+g.+6,+Padvariai,+97157"
@@ -15,13 +25,6 @@ const contacts = () => {
           >
             Kretinga, Salantų g. 6, Padvariai, 97157 Kretingos r. sav.
           </a>
-
-          <h2>Telefonas: </h2>
-
-          <div className={styles.phones}>
-            <a href="tel:+37067032769">Vadybininkas: +370 67 032 769</a>
-            <a href="tel:+37060655648">Direktorius: +370 60 655 648</a>
-          </div>
 
           <h2>Darbo valandos:</h2>
 
